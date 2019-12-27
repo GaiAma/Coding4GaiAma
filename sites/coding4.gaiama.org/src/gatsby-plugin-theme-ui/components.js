68aa2c3cbf66535bfe3cefb6cd1e2da2e518cb24
CanRau <cansrau@gmail.com>
2019-12-27 12:14:53 -0500

    feat: add deep link icon to headings

--- sites/coding4.gaiama.org/src/gatsby-plugin-theme-ui/components.js
+++ sites/coding4.gaiama.org/src/gatsby-plugin-theme-ui/components.js
@@ -31,6 +31,45 @@ import { Link } from 'components/Link'
 // )
 
 // const Paragraph = props => <Text as="p" mt="4" {...props} />
+const svgIcon = (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    height="16"
+    version="1.1"
+    viewBox="0 0 16 16"
+    width="16"
+  >
+    <path
+      fillRule="evenodd"
+      d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+    ></path>
+  </svg>
+)
+const heading = Tag => props => {
+  return (
+    <Tag
+      {...props}
+      sx={{
+        ':hover a': { display: `inline` },
+        a: {
+          ml: `-1.51rem`,
+          display: `none`,
+          '@media (hover: none)': {
+            display: `inline`,
+          },
+          pr: `0.5rem`,
+          svg: {
+            fill: `text`,
+          },
+        },
+      }}
+    >
+      <a href={`#${props.id}`}>{svgIcon}</a>
+      {props.children}
+    </Tag>
+  )
+}
 
 export default {
   // blockquote,
@@ -45,4 +84,10 @@ export default {
   // p: Paragraph,
   a: Link,
   Link,
+  h1: heading('h1'),
+  h2: heading('h2'),
+  h3: heading('h3'),
+  h4: heading('h4'),
+  h5: heading('h5'),
+  h6: heading('h6'),
 }
