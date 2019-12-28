3b80f715e47621fede3ce915dc1727955512d59a
CanRau <cansrau@gmail.com>
2019-12-28 11:14:07 -0500

    feat(Layout): favor 'mb', add skip-link
    - remove unnecessary <div>
    - move <Global> styles to theme.styles.Global

--- sites/coding4.gaiama.org/src/layouts/index.js
+++ sites/coding4.gaiama.org/src/layouts/index.js
@@ -1,73 +1,66 @@
 /** @jsx jsx */
-import { jsx, Styled } from 'theme-ui'
-import { Fragment } from 'react'
+import { jsx, Styled, Layout as UiLayout } from 'theme-ui'
+import { Box } from '@theme-ui/components'
+import { Global } from '@emotion/core'
 import { MDXProvider } from '@mdx-js/react'
-// import { FiEdit } from 'react-icons/fi'
 import { GlobalMeta } from 'components/GlobalMeta'
 import { Header } from 'components/Header'
 import { Footer } from 'components/Footer'
-
+import SkipLink from 'components/SkipLink'
 // import 'normalize.css'
 // import 'modern-normalize'
-import { Global } from '@emotion/core'
-// import { Link } from 'components/Link'
-
 // import 'typeface-cormorant-garamond'
 // import 'typeface-proza-libre'
 // import 'typeface-merriweather-sans'
 
+const isProduction = process.env.NODE_ENV === `production`
+
 // minmax(min(1fr, calc(92% - 20px)), 40rem)
-const Main = props => <div {...props} />
+// const Main = props => <div {...props} />
 
 const Layout = ({ children, data, ...props }) =>
-  (process.env.NODE_ENV !== `production` && console.log(data, props)) || (
-    <Fragment>
-      <Styled.root>
+  (!isProduction && console.log(data, props)) || (
+    <Styled.root>
+      <UiLayout>
+        <SkipLink>Skip to content</SkipLink>
         <GlobalMeta page={data?.page} meta={data?.site?.meta} />
-        <Global
-          styles={{
-            '*': {
-              boxSizing: 'border-box',
-            },
-            body: {
-              margin: 0,
-            },
-          }}
-        />
+        <Global styles={theme => theme.styles.Global} />
         <div className={`type-${data?.page?.frontmatter?.type}`}>
-          <Main>
-            <Header
-              title={data?.site?.meta.title}
-              subtitle={data?.site?.meta.description}
-              homepage={data?.homepage}
-              sx={{ gridColumn: 5 }}
-            />
+          {/* <Main> */}
+          <Header
+            title={data?.site?.meta.title}
+            subtitle={data?.site?.meta.description}
+            homepage={data?.homepage}
+            sx={{ gridColumn: 5 }}
+          />
 
+          <Box id="content" mb="10">
             <MDXProvider>{children}</MDXProvider>
+          </Box>
 
-            <Footer
-              mt="4"
-              mb="3"
-              variant="styles.Footer"
-              meta={data?.site?.meta}
-              menu={data?.footerMenu?.nodes}
-              editLink={data?.page?.fields?.editLink}
-              // additionalLinks={[
-              //   {
-              //     url: data?.page?.fields?.editLink,
-              //     title: (
-              //       <span>
-              //         <FiEdit />
-              //         Edit
-              //       </span>
-              //     ),
-              //   },
-              // ]}
-            />
-          </Main>
+          <Footer
+            mt="4"
+            mb="3"
+            variant="styles.Footer"
+            meta={data?.site?.meta}
+            menu={data?.footerMenu?.nodes}
+            editLink={data?.page?.fields?.editLink}
+            // additionalLinks={[
+            //   {
+            //     url: data?.page?.fields?.editLink,
+            //     title: (
+            //       <span>
+            //         <FiEdit />
+            //         Edit
+            //       </span>
+            //     ),
+            //   },
+            // ]}
+          />
+          {/* </Main> */}
         </div>
-      </Styled.root>
-    </Fragment>
+      </UiLayout>
+    </Styled.root>
   )
 
 export default Layout
