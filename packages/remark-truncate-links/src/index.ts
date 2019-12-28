d58abdb5f44f34e522741d49b3e87cd9ac5570e6
CanRau <cansrau@gmail.com>
2019-12-28 10:54:14 -0500

    feat(remark-truncate-links): add/improve truncate options

--- packages/remark-truncate-links/src/index.ts
+++ packages/remark-truncate-links/src/index.ts
@@ -1,19 +1,50 @@
 import { Node } from 'unist'
 import visit from 'unist-util-visit'
 import { truncateMiddle } from 'autolinker/dist/commonjs/truncate/truncate-middle.js'
+import { truncateSmart } from 'autolinker/dist/commonjs/truncate/truncate-smart.js'
+
+type Props = {
+  style?: string
+  length?: number
+}
 
 type NodeWithChildren = Node & {
   children?: Node[]
   value?: string
 }
 
-export function remarkTruncateLinks(): (ast: NodeWithChildren) => void {
+type TruncateStyle = {
+  [key: string]: (
+    url: string,
+    truncateLen: number,
+    ellipsisChars?: string
+  ) => string
+}
+
+const truncators: TruncateStyle = {
+  smart: truncateSmart,
+  middle: truncateMiddle,
+}
+
+export function remarkTruncateLinks({
+  style = `smart`,
+  length = 30,
+}: Props = {}): (ast: NodeWithChildren) => void {
+  const truncator = truncators[style]
+
+  if (typeof truncator !== `function`) {
+    const supportedStyles = Object.keys(truncators).join(`, `)
+    throw new Error(
+      `Please select one of the supported styles [${supportedStyles}]`
+    )
+  }
+
   return function(ast: NodeWithChildren): void {
     visit(ast, 'link', (node: NodeWithChildren) => {
       node.title = node.title || node.url
       node.children?.map((child: NodeWithChildren): void => {
         if (typeof child?.value === `string` && child.value === node.url) {
-          child.value = truncateMiddle(child.value, 30, `...`)
+          child.value = truncator(child.value, length, `...`)
         }
       })
     })
