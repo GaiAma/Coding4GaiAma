a0a9085b308787db1e8c73df76690f5c77f4592f
CanRau <cansrau@gmail.com>
2019-12-30 00:37:17 -0500

    fix: remove console.log

--- sites/coding4.gaiama.org/src/templates/NotFound.js
+++ sites/coding4.gaiama.org/src/templates/NotFound.js
@@ -12,34 +12,31 @@ import { Link } from 'components/Link'
  * https://www.shivarweb.com/13491/404-page-best-practices/
  * https://www.cludo.com/blog/best-practices-for-brilliant-404-pages/
  */
-const NotFound = props => {
-  console.log(props.location)
-  return (
-    <Box variant="grid">
-      {!!props?.data?.page?.frontmatter?.title && (
-        <h1>{props?.data?.page.frontmatter.title}</h1>
-      )}
-      {!!props?.data?.page?.body && (
-        <MDXRenderer>{props?.data?.page.body}</MDXRenderer>
-      )}
-      <Box
-        mb="7"
-        px="6"
-        py="1"
-        sx={{ color: `background`, bg: `text`, borderRadius: `round` }}
-      >
-        <span>{props.location.host}</span>
-        <span sx={{ fontWeight: `bold` }}>{props.location.pathname}</span>
-      </Box>
-      <Box>
-        Head back to{' '}
-        <Link to={props.data.homepage.fields.url}>
-          {props.data.homepage.frontmatter.title}
-        </Link>
-      </Box>
+const NotFound = props => (
+  <Box variant="grid">
+    {!!props?.data?.page?.frontmatter?.title && (
+      <h1>{props?.data?.page.frontmatter.title}</h1>
+    )}
+    {!!props?.data?.page?.body && (
+      <MDXRenderer>{props?.data?.page.body}</MDXRenderer>
+    )}
+    <Box
+      mb="7"
+      px="6"
+      py="1"
+      sx={{ color: `background`, bg: `text`, borderRadius: `round` }}
+    >
+      <span>{props.location.host}</span>
+      <span sx={{ fontWeight: `bold` }}>{props.location.pathname}</span>
     </Box>
-  )
-}
+    <Box>
+      Head back to{' '}
+      <Link to={props.data.homepage.fields.url}>
+        {props.data.homepage.frontmatter.title}
+      </Link>
+    </Box>
+  </Box>
+)
 
 export default NotFound
 
