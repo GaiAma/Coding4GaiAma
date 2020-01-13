220dc5338a507d982191c95fb8d1562993d921f5
CanRau <cansrau@gmail.com>
2020-01-13 01:27:07 -0500

    build: update env vars for GH Action

--- sites/coding4.gaiama.org/src/utils/environment-helpers.js
+++ sites/coding4.gaiama.org/src/utils/environment-helpers.js
@@ -1,26 +1,27 @@
-const { homepage, version, repository } = require(`../../package.json`)
+const { homepage, repository, bugs } = require(`../../package.json`)
 
 const {
   URL: NETLIFY_SITE_URL = homepage,
   DEPLOY_PRIME_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = process.env.NODE_ENV,
   BRANCH,
+  GITHUB_REF,
 } = process.env
-const branch = BRANCH || `dev`
-const isNetlifyProduction =
-  NETLIFY_ENV === `production` || `${branch}`.startsWith(`ab_`)
-const siteUrl = isNetlifyProduction
-  ? NETLIFY_SITE_URL
-  : DEPLOY_PRIME_URL || homepage
+
+const branch = BRANCH || GITHUB_REF || `dev`
 const isDebug = /^(gatsby)?\*/i.test(`${process.env.DEBUG}`)
+const siteUrl = isProduction ? NETLIFY_SITE_URL : DEPLOY_PRIME_URL
+const isProduction =
+  NETLIFY_ENV === `production` || `${branch}`.startsWith(`ab_`)
 
 module.exports = {
-  BRANCH,
   branch,
-  DEPLOY_PRIME_URL,
   isDebug,
-  isNetlifyProduction,
-  NETLIFY_ENV,
-  NETLIFY_SITE_URL,
+  isProduction,
   siteUrl,
+  repository: {
+    url: `${repository.url}`.replace(/\.git$/, ``),
+    directory: repository.directory,
+  },
+  newIssueUrl: bugs.url + '/new',
 }
