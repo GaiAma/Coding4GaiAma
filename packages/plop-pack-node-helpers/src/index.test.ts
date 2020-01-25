2a5892f3e65b3d213c5e6f0fd33e9d9435f1dfa0
CanRau <cansrau@gmail.com>
2020-01-25 16:34:57 -0500

    build: init plop-pack-gatsby & plop-pack-node-helpers

---
+++ packages/plop-pack-node-helpers/src/index.test.ts
@@ -0,0 +1,10 @@
+import test, { ExecutionContext } from 'ava';
+import plopPackNodeHelpers from './index';
+
+const clearTerminal = () => process.stdout.write('\x1B[2J\x1B[3J\x1B[H\x1Bc');
+
+test.before((t: ExecutionContext) => {
+  clearTerminal();
+});
+
+test('plop-pack-node-helpers', (t: ExecutionContext) => {});
