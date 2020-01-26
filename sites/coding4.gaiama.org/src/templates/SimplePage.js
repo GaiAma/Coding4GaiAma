8bc31f2d91fc777cc666af329d4137732b8d67a8
CanRau <cansrau@gmail.com>
2020-01-25 22:18:21 -0500

    feat: add toc to SimplePage & Article

--- sites/coding4.gaiama.org/src/templates/SimplePage.js
+++ sites/coding4.gaiama.org/src/templates/SimplePage.js
@@ -3,10 +3,49 @@ import { jsx } from 'theme-ui'
 import { graphql } from 'gatsby'
 import { Box } from '@theme-ui/components'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link } from '../components/Link'
+
+// inspired by https://gatsby-theme-legals.netlify.com/privacy-policy
+const TableOfContents = ({ items }) =>
+  !!items.length && (
+    <ul>
+      {items.map(item => (
+        <li key={item.url}>
+          <Link to={item.url}>{item.title}</Link>
+          {!!item.children?.length && <TableOfContents items={item.children} />}
+        </li>
+      ))}
+    </ul>
+  )
 
 const SimplePage = ({ data: { page }, ...props }) => {
+  // does that always work?
+  const toc = page.frontmatter.showTableOfContents === true ? page.toc : []
   return (
     <Box variant="grid">
+      {!!toc?.length && (
+        <Box
+          sx={{
+            '@media screen and (min-width: 1030px)': {
+              gridRow: '1',
+              gridColumn: '1/5',
+              position: 'sticky',
+              top: 50,
+              ml: 5,
+            },
+          }}
+        >
+          <Box
+            sx={{
+              '@media screen and (min-width: 1030px)': { position: `absolute` },
+            }}
+          >
+            <h4 sx={{ textTransform: `uppercase` }}>Table of contents</h4>
+            <TableOfContents items={toc} />
+          </Box>
+        </Box>
+      )}
+
       <MDXRenderer>{page.body}</MDXRenderer>
     </Box>
   )
@@ -23,7 +62,10 @@ export const query = graphql`
       frontmatter: { layout: { eq: "SimplePage" } }
     ) {
       ...CommonFields
-      tableOfContents
+      toc
+      frontmatter {
+        showTableOfContents
+      }
     }
   }
 `
