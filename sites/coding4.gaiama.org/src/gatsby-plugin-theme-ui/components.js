c87557ab0bd8c79558b9b11499f95dfb3c67ea5d
CanRau <cansrau@gmail.com>
2020-02-04 12:48:52 -0500

    style: cleanup

--- sites/coding4.gaiama.org/src/gatsby-plugin-theme-ui/components.js
+++ sites/coding4.gaiama.org/src/gatsby-plugin-theme-ui/components.js
@@ -5,31 +5,37 @@ import { Box } from '@theme-ui/components'
 import { Link } from 'components/Link'
 import { Heading } from 'components/Heading'
 
-// const blockquote = props => (
-//   <blockquote
-//     sx={{
-//       gridColumn: `4/7`,
-//       width: `100%`,
-//       maxWidth: `100%`,
-//       paddingLeft: `10px`,
-//     }}
-//     {...props}
-//   />
-// )
-
 const gridSize = str => {
   const result = str ?? `grid.contentL`
   return !/^grid\./.test(result) ? `grid.${result}` : result
 }
 
+const pre = props => (
+  <div
+    {...props}
+    sx={{ variant: gridSize(props.children?.props?.size), mb: 10 }}
+  />
+)
+
+const table = ({ headers = [], ...props }) => (
+  <table
+    {...props}
+    sx={{
+      ...(headers?.length && {
+        '@media only screen and (max-width: 900px)': headers.reduce(
+          (result, each, index) => ({
+            ...result,
+            [`td:nth-of-type(${index + 1}):before`]: { content: `"${each}"` },
+          }),
+          {}
+        ),
+      }),
+    }}
+  />
+)
+
 export default {
-  // blockquote,
-  pre: props => (
-    <div
-      {...props}
-      sx={{ variant: gridSize(props.children?.props?.size), mb: 10 }}
-    />
-  ),
+  pre,
   code: props => <CodeBlock {...props} />,
   a: Link,
   Link,
@@ -40,27 +46,8 @@ export default {
   h5: props => <Heading {...props} as="h5" />,
   h6: props => <Heading {...props} as="h6" />,
   Box: props => <Box mb="10" {...props} />,
-  table: ({ headers = [], ...props }) => (
-    <table
-      {...props}
-      sx={{
-        ...(headers?.length && {
-          '@media only screen and (max-width: 900px)': headers.reduce(
-            (result, each, index) => ({
-              ...result,
-              [`td:nth-of-type(${index + 1}):before`]: { content: `"${each}"` },
-            }),
-            {}
-          ),
-        }),
-      }}
-    />
-  ),
-  // TODO: hmmm don't wrap? how are gatsbyjs.org's tables responsive??
-  // <Box as="figure" sx={{ overflowX: `auto`, variant: `grid.contentL` }}>
-  // </Box>
+  table,
   kbd: props => <kbd {...props} sx={{ variant: `styles.kbd` }} />,
   sup: 'sup',
   figure: 'figure',
-  // div: 'div',
 }
